3ec87b243d6e203fabb500cada434f45cf1df902
Michael Samon <michael.samon11@gmail.com>
2019-02-06 19:23:37 -0700

    added weight matrix initialization and Dense

--- neuralnet.py
+++ neuralnet.py
@@ -12,49 +12,105 @@ import numpy as np
 from utils import NeuralError
 
 
-class Layer:
+class Network:
     """
     Handles matrix shaping and initialization.
     """
-    def __init__(self, input_shape=None):
-        self.INPUT_SHAPE = input_shape
+    def __init__(self):
         self.training = False
 
-        # list containing layer widths in order of graph definition
+        # list containing layer objects in order of graph definition
         self.layers = []
+
+        # list of supported activations
+        self.activations = ['tanh', 'sigmoid', 'relu']
+
+        # list containing the successive shapes of network layers
+        self.shapes = []
+
+        self.weight_matrices = []
 
     def get_matrix_shapes(self):
         """
-        Return a list of all layer shapes with the input matrix
+        Computes a list of all layer shapes with the input matrix
         shape at the start index and the output matrix shape
         at the end index.
         """
 
-        # input_shape must have been set by this point
-        if self.INPUT_SHAPE is None:
-            raise NeuralError("'input_shape' was found to be None")
-
-        shapes = []
-
         if len(self.layers) < 1:
             raise NeuralError(
-                "Not enough layers to initialize graph",
-                data=self.layers)
+                "Not enough layers to initialize graph found",
+                data=len(self.layers))
 
-        for i, width in enumerate(self.layers):
-            if i == 0:  # input matrix
-                shape = (self.INPUT_SHAPE[0], width)
-                shapes.append(shape)
+        for i, layer in enumerate(self.layers):
+            # input matrix
+            if i == 0:
+                if layer.INPUT_SHAPE is None:
+                    raise NeuralError(
+                        "input_shape not specified for first layer got None"
+                        )
+
+                shape = (layer.INPUT_SHAPE[0], layer.width)
+                self.shapes.append(shape)
+
             else:
                 # set columns of preceding matrix as row
                 # size of current matrix
-                shape = (shapes[i-1][1], width)
-                shapes.append(shape)
+                shape = (self.shapes[i-1][1], layer.width)
+                self.shapes.append(shape)
 
-        return shapes
+        self.shapes = self.shapes
+
+    def add(self, layer, activation=None):
+        """Add layer to the network graph."""
+
+        # check that a layer object was passed in
+        try:
+            getattr(layer, "width")
+        except AttributeError:
+            raise NeuralError(
+                "'layer' argument is not a layer object instead got",
+                data=type(layer))
+
+        self.layers.append(layer)
+
+        # index 0 represents the input matrix
+        layer.layer_index = len(self.layers) - 1
+
+    def compile_graph(self):
+        """Initialize layer matrices and their weights."""
+        self.get_matrix_shapes()
+        for layer in self.layers:
+            shape = self.shapes[layer.layer_index]
+            random_matrix = np.random.rand(shape[0], shape[1])
+            layer.weights = random_matrix
+
+            self.weight_matrices.append(random_matrix)
+
+
+class Dense:
+    """Class that defines Dense network layer attributes."""
+
+    def __init__(self, width, activation=None, input_shape=None):
+        self.INPUT_SHAPE = input_shape
+
+        self.width = width
+        self.activation = activation
+
+        # layer index in the graph
+        self.layer_index = 0
+
+        # tuple containing the layer's weight matrix shape
+        self.shape = None
+
+        # the weight matrix
+        self.weights = None
 
 
 if __name__ == "__main__":
-    network = Layer()
-    network.layers = [6, 6, 1]
-    print(network.get_matrix_shapes())
+    network = Network()
+    network.add(Dense(6, input_shape=(6,)))
+    network.add(Dense(16))
+    network.add(Dense(1))
+    network.compile_graph()
+    print([layer.shape for layer in network.weight_matrices])
