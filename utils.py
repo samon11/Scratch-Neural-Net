64464e7c7b99267aaa5788e1d460bb15fe09ebeb
Michael Samon <michael.samon11@gmail.com>
2019-02-06 19:24:15 -0700

    made data argument more readable

--- utils.py
+++ utils.py
@@ -13,6 +13,6 @@ class NeuralError(Exception):
         self.data = data
 
     def __str__(self):
-        error_data = "::" + repr(self.data) if self.data is not None else ""
+        error_data = " {}".format(self.data) if self.data is not None else ""
         error_message = self.message + error_data
         return error_message
