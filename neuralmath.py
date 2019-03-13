4361765130a3e54ab7de1398d9ce12e838832cd3
Michael Samon <mjsamon@protonmail.com>
2019-03-13 12:32:07 -0600

    compress activation derivs

--- neuralmath.py
+++ neuralmath.py
@@ -10,23 +10,23 @@ import numpy as np
 class Activations:
     """Class that handles activation funcs of the neural network"""
 
-    def sigmoid(x):
-        return (1 / (1 + np.exp(-1 * x)))
+    def sigmoid(x, deriv=False):
+        if not deriv:
+            return (1 / (1 + np.exp(-1 * x)))
+        else:
+            return np.exp(-1 * x) / ((1 / (1 + np.exp(-1 * x))) ** 2)
 
-    def d_sigmoid(x):
-        return np.exp(-1 * x) / ((1 / (1 + np.exp(-1 * x))) ** 2)
+    def relu(x, deriv=False):
+        if not deriv:
+            return np.maximum(0, x)
+        else:
+            return (x > 0) * 1
 
-    def relu(x):
-        return np.maximum(0, x)
-
-    def d_relu(x):
-        return (x > 0) * 1
-
-    def tanh(x):
-        return np.tanh(x)
-
-    def d_tanh(x):
-        return 1 - (np.tanh(x) ** 2)
+    def tanh(x, deriv=False):
+        if not deriv:
+            return np.tanh(x)
+        else:
+            return 1 - (np.tanh(x) ** 2)
 
 
 if __name__ == "__main__":
